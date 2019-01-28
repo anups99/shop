303a1e754b63e1ef25eed9fd475e1ded1b415437
anups99 <kanchha.anup1@gmail.com>
2019-01-28 22:33:11 +0545

    First Commit

---
+++ config/routes.rb
@@ -0,0 +1,47 @@
+Rails.application.routes.draw do
+  root'home#index'
+
+
+get 'signup', to: 'users#new' , as: 'signup'
+get 'login', to: 'sessions#new' , as: 'login'
+get 'logout', to: 'sessions#destroy' , as: 'logout'
+resources :users
+resources :sessions, only: [:new, :create, :destroy]
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
